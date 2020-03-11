a62bff3e5cf7a6caa5b50bdad1792faef461db55
Abirami N <abirami_n@dell.com>
2020-03-11 12:30:17 +0000

    Initial_commit

---
+++ plugins/action/sonic.py
@@ -0,0 +1,52 @@
+#
+# (c) 2016 Red Hat Inc.
+#
+# (c) 2019 Dell EMC.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+#
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import sys
+import copy
+
+from ansible import constants as C
+from ansible.plugins.action.network import ActionModule as ActionNetworkModule
+from ansible.module_utils.network.common.utils import load_provider
+
+from ansible.utils.display import Display
+
+display = Display()
+
+
+class ActionModule(ActionNetworkModule):
+
+    def run(self, tmp=None, task_vars=None):
+        del tmp  # tmp no longer has any effect
+
+        module_name = self._task.action.split('.')[-1]
+        self._config_module = True if module_name == 'sonic_config' else False
+        persistent_connection = self._play_context.connection.split('.')[-1]
+
+        if self._play_context.connection in ('network_cli', 'httpapi'):
+            provider = self._task.args.get('provider', {})
+            if any(provider.values()):
+                display.warning('provider is unnecessary when using network_cli and will be ignored')
+                del self._task.args['provider']
+
+        result = super(ActionModule, self).run(task_vars=task_vars)
+        return result
