0c369b38702261e8b5c069162cdc5bb1c9689e19
Shade Talabi <shade.talabi@dell.com>
2022-03-14 12:56:32 -0700

    fixed sanity check errors

--- plugins/httpapi/sonic.py
+++ plugins/httpapi/sonic.py
@@ -22,8 +22,8 @@ __metaclass__ = type
 
 DOCUMENTATION = """
 ---
-author: Ansible Networking Team
-module: sonic
+author: Dell
+name: sonic
 short_description: HttpApi Plugin for devices supporting Restconf SONIC API
 description:
   - This HttpApi plugin provides methods to connect to Restconf SONIC API endpoints.
