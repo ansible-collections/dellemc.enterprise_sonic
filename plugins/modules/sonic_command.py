92c16d1b31c06c290568846b4c6b2ad679faa6d1
Kerry Meyer <kerry@kerry-ubuntu-20.04.1>
2022-04-22 15:51:13 -0700

    Fix problems in handling of CLI "exec" mode and "config" mode commands.
    Root cause analysis:
    
    Commands with a "prompt/answer" sequence require
    the user to specify a dictionary containing the command, prompt,
    and prompt "answer". The previously existing handling enclosed the
    entire (quoted) command entry under a single "command" dictionary
    item for the dictionary that is sent to the switch instead of
    setting the required specified values for each of the three
    dictionary items.
    
    Fix:
     - Create a utility function to generate the correct dictionary
       entries from the input command list parsed from the user
       playbook.
     - Use the resulting dictionary instead of the "raw" list
       of quoted command strings provided by the "AnsibleModule"
       parsing invocation.

--- plugins/modules/sonic_command.py
+++ plugins/modules/sonic_command.py
@@ -141,24 +141,31 @@ from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.p
     Conditional,
 )
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.utils import (
-    transform_commands,
+    EntityCollection,
     to_lines,
 )
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.sonic import run_commands
+from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.utils.utils import command_list_str_to_dict
 
+def transform_commands_dict(module, commands_dict):
+    transform = EntityCollection(
+        module,
+        dict(
+            command=dict(key=True),
+            output=dict(),
+            prompt=dict(type="list"),
+            answer=dict(type="list"),
+            newline=dict(type="bool", default=True),
+            sendonly=dict(type="bool", default=False),
+            check_all=dict(type="bool", default=False),
+        ),
+    )
+
+    return transform(commands_dict)
 
 def parse_commands(module, warnings):
-    commands = transform_commands(module)
-
-    if module.check_mode:
-        for item in list(commands):
-            if not item['command'].startswith('show'):
-                warnings.append(
-                    'Only show commands are supported when using check mode, not '
-                    'executing %s' % item['command']
-                )
-                commands.remove(item)
-
+    commands_dict = command_list_str_to_dict(module, warnings, module.params["commands"])
+    commands = transform_commands_dict(module, commands_dict)
     return commands
 
 
