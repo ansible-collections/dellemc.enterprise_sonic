d4ec49fbd041da93436967f6f601ee7257401ab7
stalabi1 <54641848+stalabi1@users.noreply.github.com>
2022-05-10 08:11:48 -0700

    Added attributes to bgp_neighbors module (#72)
    * Added attributes to bgp_neighbors module
    
    * resolved sanity check error
    
    * resolved sanity check
    
    * Added deletion of encrypted password to test cases
    
    * Resolved sanity check error
    
    * removed space
    
    * removed space
    
    * edited delete example
    
    * updated delete example
    
    * Updated delete test cases
    
    * 1.Added BFD, shutdown-message, and connect-retry attributes 2.Fixed test case deletion issue
    
    * resolved sanity check errors
    
    * resolved sanity check errors
    
    * updated example
    
    Co-authored-by: Shade Talabi <shade.talabi@dell.com>

--- plugins/module_utils/network/sonic/facts/bgp_neighbors/bgp_neighbors.py
+++ plugins/module_utils/network/sonic/facts/bgp_neighbors/bgp_neighbors.py
@@ -41,13 +41,34 @@ class Bgp_neighborsFacts(object):
         'peer_group': 'peer-group',
         'keepalive': 'keepalive-interval',
         'holdtime': 'hold-time',
+        'connect_retry': 'connect-retry',
         'advertisement_interval': 'minimum-advertisement-interval',
-        'bfd': ['enable-bfd', 'enabled'],
+        'bfd_enabled': ['enable-bfd', 'enabled'],
+        'check_failure': ['enable-bfd', 'check-control-plane-failure'],
+        'profile': ['enable-bfd', 'bfd-profile'],
         'dynamic': 'capability-dynamic',
         'extended_nexthop': 'capability-extended-nexthop',
         'pwd': ['auth-password', 'password'],
         'encrypted': ['auth-password', 'encrypted'],
         'nbr_description': 'description',
+        'disable_connected_check': 'disable-ebgp-connected-route-check',
+        'dont_negotiate_capability': 'dont-negotiate-capability',
+        'enforce_first_as': 'enforce-first-as',
+        'enforce_multihop': 'enforce-multihop',
+        'local_address': ['transport', 'config', 'local-address'],
+        'as': 'local-as',
+        'no_prepend': 'local-as-no-prepend',
+        'replace_as': 'local-as-replace-as',
+        'override_capability': 'override-capability',
+        'port': 'peer-port',
+        'shutdown_msg': 'shutdown-message',
+        'solo': 'solo-peer',
+        'strict_capability_match': 'strict-capability-match',
+        'ttl_security': 'ttl-security-hops',
+        'enabled': ['ebgp-multihop', 'enabled'],
+        'multihop_ttl': ['ebgp-multihop', 'multihop-ttl'],
+        'v6only': 'openconfig-bgp-ext:v6only',
+        'passive': ['transport', 'config', 'passive-mode']
     }
 
     def __init__(self, module, subspec='config', options='options'):
@@ -157,8 +178,49 @@ class Bgp_neighborsFacts(object):
                     if encrypted is not None:
                         auth_pwd['encrypted'] = encrypted
                         fil_neighbor.pop('encrypted')
+                    ebgp_multihop = {}
+                    enabled = fil_neighbor.get('enabled', None)
+                    if enabled is not None:
+                        ebgp_multihop['enabled'] = enabled
+                        fil_neighbor.pop('enabled')
+                    multihop_ttl = fil_neighbor.get('multihop_ttl', None)
+                    if multihop_ttl is not None:
+                        ebgp_multihop['multihop_ttl'] = multihop_ttl
+                        fil_neighbor.pop('multihop_ttl')
+                    local_as = {}
+                    asn = fil_neighbor.get('as', None)
+                    if asn is not None:
+                        local_as['as'] = asn
+                        fil_neighbor.pop('as')
+                    no_prepend = fil_neighbor.get('no_prepend', None)
+                    if no_prepend is not None:
+                        local_as['no_prepend'] = no_prepend
+                        fil_neighbor.pop('no_prepend')
+                    replace_as = fil_neighbor.get('replace_as', None)
+                    if replace_as is not None:
+                        local_as['replace_as'] = replace_as
+                        fil_neighbor.pop('replace_as')
+                    bfd = {}
+                    bfd_enabled = fil_neighbor.get('bfd_enabled', None)
+                    if bfd_enabled is not None:
+                        bfd['enabled'] = bfd_enabled
+                        fil_neighbor.pop('bfd_enabled')
+                    check_failure = fil_neighbor.get('check_failure', None)
+                    if check_failure is not None:
+                        bfd['check_failure'] = check_failure
+                        fil_neighbor.pop('check_failure')
+                    profile = fil_neighbor.get('profile', None)
+                    if profile is not None:
+                        bfd['profile'] = profile
+                        fil_neighbor.pop('profile')
                     if auth_pwd:
                         fil_neighbor['auth_pwd'] = auth_pwd
+                    if ebgp_multihop:
+                        fil_neighbor['ebgp_multihop'] = ebgp_multihop
+                    if local_as:
+                        fil_neighbor['local_as'] = local_as
+                    if bfd:
+                        fil_neighbor['bfd'] = bfd
                     if fil_neighbor:
                         fil_neighbors.append(fil_neighbor)
             if fil_neighbors:
