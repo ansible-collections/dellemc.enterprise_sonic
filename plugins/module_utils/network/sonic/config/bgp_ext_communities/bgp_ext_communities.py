0f0962d04f1fbc7ea247d0b857d883fe269358e9
stalabi1 <54641848+stalabi1@users.noreply.github.com>
2022-09-07 13:02:29 -0700

    Fixed regex to support standard naming with port breakout mode and resolved other minor bugs (#110)
    * Fixed regex to support standard naming with port breakout mode and resolved other minor bugs
    
    * Resolved sanity check error
    
    * Resolved sanity check error
    
    * Reverted l2 interface standard naming bug fix
    
    * Removed dead code
    
    Co-authored-by: Shade Talabi <shade.talabi@dell.com>

--- plugins/module_utils/network/sonic/config/bgp_ext_communities/bgp_ext_communities.py
+++ plugins/module_utils/network/sonic/config/bgp_ext_communities/bgp_ext_communities.py
@@ -328,7 +328,7 @@ class Bgp_ext_communities(ConfigBase):
                                     ]
                                 }
                             }"""
-        env = jinja2.Environment(autoescape=False, extensions=['jinja2.ext.autoescape'])
+        env = jinja2.Environment(autoescape=False)
         t = env.from_string(payload_template)
         intended_payload = t.render(input_data)
         ret_payload = json.loads(intended_payload)
