316970b90fd372c2940febe85ad7be82073bf138
mingjun-ansible <mingjun_zhang@dell.com>
2022-10-26 10:01:36 -0700

    Add interface speed and more attributes

--- plugins/modules/sonic_interfaces.py
+++ plugins/modules/sonic_interfaces.py
@@ -63,6 +63,45 @@ options:
         description:
         - MTU of the interface.
         type: int
+      speed:
+        description:
+        - Interface speed.
+        - Supported speeds are dependent on the type of switch.
+        type: str
+        choices:
+        - SPEED_10MB
+        - SPEED_100MB
+        - SPEED_1GB
+        - SPEED_2500MB
+        - SPEED_5GB
+        - SPEED_10GB
+        - SPEED_20GB
+        - SPEED_25GB
+        - SPEED_40GB
+        - SPEED_50GB
+        - SPEED_100GB
+        - SPEED_400GB
+      auto_negotiate:
+        description:
+        - auto-negotiate transmission parameters with peer interface.
+        type: bool
+      advertised_speed:
+        description:
+        - Advertised speeds of the interface.
+        - Supported speeds are dependent on the type of switch.
+        - Speeds may be 10, 100, 1000, 2500, 5000, 10000, 20000, 25000, 40000, 50000, 100000 or 400000.
+        type: list
+        elements: str
+      fec:
+        description:
+        - Interface FEC (Forward Error Correction).
+        type: str
+        choices:
+        - FEC_RS
+        - FEC_FC
+        - FEC_DISABLED
+        - FEC_DEFAULT
+        - FEC_AUTO
   state:
     description:
     - The state the configuration should be left in.
@@ -80,18 +119,27 @@ EXAMPLES = """
 #
 # show interface status | no-more
 #------------------------------------------------------------------------------------------
-#Name                Description         Admin          Oper           Speed          MTU
+#Name                Description         Admin     Oper      AutoNeg     Speed        MTU
 #------------------------------------------------------------------------------------------
-#Eth1/1           -                   up                            100000         9100
-#Eth1/2           -                   up                            100000         9100
-#Eth1/3           -                   down                          100000         9100
-#Eth1/3           -                   down                          1000           5000
-#Eth1/5           -                   down                          100000         9100
+#Ethernet0           -                   up                              100000       9100
+#Ethernet4           -                   up                              100000       9100
+#Ethernet8           -                   down                            100000       9100
+#Ethernet12          Ethernet-12         down                on          -            5000
+#Ethernet16          -                   down                            40000        9100
 #
-- name: Configures interfaces
-  dellemc.enterprise_sonic.sonic_interfaces:
+# show running-configuration interface Ethernet 8
+#!
+#interface Ethernet8
+# mtu 9100
+# speed 100000
+# fec AUTO
+# shutdown
+#
+- name: Configure interfaces
+  sonic_interfaces:
     config:
-      name: Eth1/3
+      - name: Ethernet12
+      - name: Ethernet16
     state: deleted
 #
 # After state:
@@ -99,14 +147,20 @@ EXAMPLES = """
 #
 # show interface status | no-more
 #------------------------------------------------------------------------------------------
-#Name                Description         Admin          Oper           Speed          MTU
+#Name                Description         Admin     Oper      AutoNeg     Speed        MTU
 #------------------------------------------------------------------------------------------
-#Eth1/1           -                   up                            100000         9100
-#Eth1/2           -                   up                            100000         9100
-#Eth1/3           -                   down                          100000         9100
-#Eth1/3           -                   up                            100000         9100
-#Eth1/5           -                   down                          100000         9100
+#Ethernet0           -                   up                              100000       9100
+#Ethernet4           -                   up                              100000       9100
+#Ethernet8           -                   down                            100000       9100
+#Ethernet12          -                   up                              100000       9100
+#Ethernet16          -                   down                            100000       9100
 #
+# show running-configuration interface Ethernet 8
+#!
+#interface Ethernet8
+# mtu 9100
+# speed 100000
+# shutdown
 #
 # Using deleted
 #
@@ -115,33 +169,33 @@ EXAMPLES = """
 #
 # show interface status | no-more
 #------------------------------------------------------------------------------------------
-#Name                Description         Admin          Oper           Speed          MTU
+#Name                Description         Admin     Oper      AutoNeg     Speed        MTU
 #------------------------------------------------------------------------------------------
-#Eth1/1           -                   up                            100000         9100
-#Eth1/2           -                   up                            100000         9100
-#Eth1/3           -                   down                          100000         9100
-#Eth1/3           -                   down                          1000           9100
-#Eth1/5           -                   down                          100000         9100
+#Ethernet0           -                   up                              100000       9100
+#Ethernet4           -                   up                              100000       9100
+#Ethernet8           -                   down                            100000       9100
+#Ethernet12          -                   down                            1000         9100
+#Ethernet16          -                   down                            100000       9100
 #
-
-- name: Configures interfaces
-  dellemc.enterprise_sonic.sonic_interfaces:
+- name: Configure interfaces
+  sonic_interfaces:
     config:
-    state: deleted
 
+    state: deleted
 #
 # After state:
 # -------------
 #
 # show interface status | no-more
 #------------------------------------------------------------------------------------------
-#Name                Description         Admin          Oper           Speed          MTU
+#Name                Description         Admin     Oper      AutoNeg     Speed        MTU
 #------------------------------------------------------------------------------------------
-#Eth1/1           -                   up                            100000         9100
-#Eth1/2           -                   up                            100000         9100
-#Eth1/3           -                   up                            100000         9100
-#Eth1/3           -                   up                            100000         9100
-#Eth1/5           -                   up                            100000         9100
+#Ethernet0           -                   up                              100000       9100
+#Ethernet4           -                   up                              100000       9100
+#Ethernet8           -                   up                              100000       9100
+#Ethernet12          -                   up                              100000       9100
+#Ethernet16          -                   up                              100000       9100
+#
 #
 #
 # Using merged
@@ -151,38 +205,175 @@ EXAMPLES = """
 #
 # show interface status | no-more
 #------------------------------------------------------------------------------------------
-#Name                Description         Admin          Oper           Speed          MTU
+#Name                Description         Admin     Oper      AutoNeg     Speed        MTU
 #------------------------------------------------------------------------------------------
-#Eth1/1           -                   up                            100000         9100
-#Eth1/2           -                   up                            100000         9100
-#Eth1/3           -                   down                          100000         9100
-#Eth1/3           -                   down                          1000           9100
+#Ethernet0           -                   up                              100000       9100
+#Ethernet4           -                   up                              100000       9100
+#Ethernet8           -                   down                            100000       9100
+#Ethernet12          -                   down                            1000         9100
 #
-- name: Configures interfaces
-  dellemc.enterprise_sonic.sonic_interfaces:
+# show running-configuration interface Ethernet 8
+#!
+#interface Ethernet8
+# mtu 9100
+# speed 100000
+# shutdown
+#
+- name: Configure interfaces
+  sonic_interfaces:
     config:
-     - name: Eth1/3
-       description: 'Ethernet Twelve'
-     - name: Eth1/5
-       description: 'Ethernet Sixteen'
-       enable: True
-       mtu: 3500
+      - name: Ethernet8
+        fec: FEC_AUTO
+      - name: Ethernet12
+        description: 'Ethernet Twelve'
+        auto_negotiate: True
+      - name: Ethernet16
+        description: 'Ethernet Sixteen'
+        enabled: True
+        mtu: 3500
+        speed: SPEED_40GB
     state: merged
-#
 #
 # After state:
 # ------------
 #
 # show interface status | no-more
 #------------------------------------------------------------------------------------------
-#Name                Description         Admin          Oper           Speed          MTU
+#Name                Description         Admin     Oper      AutoNeg     Speed        MTU
 #------------------------------------------------------------------------------------------
-#Eth1/1           -                   up                            100000         9100
-#Eth1/2           -                   up                            100000         9100
-#Eth1/3           -                   down                          100000         9100
-#Eth1/4           -                   down                          1000           9100
-#Eth1/5           -                   down                          100000         3500
+#Ethernet0           -                   up                              100000       9100
+#Ethernet4           -                   up                              100000       9100
+#Ethernet8           -                   down                            100000       9100
+#Ethernet12          Ethernet Twelve     down                on          -            9100
+#Ethernet16          Ethernet Sixteen    down                            40000        3500
 #
+# show running-configuration interface Ethernet 8
+#!
+#interface Ethernet8
+# mtu 9100
+# speed 100000
+# fec AUTO
+# shutdown
+#
+# Using overridden
+#
+# Before state:
+# -------------
+#
+# show interface status | no-more
+#------------------------------------------------------------------------------------------
+#Name                Description         Admin     Oper      AutoNeg     Speed        MTU
+#------------------------------------------------------------------------------------------
+#Ethernet0           E0                  up                              100000       9100
+#Ethernet4           E4                  up                              100000       9100
+#Ethernet8           E8                  down                            100000       9100
+#Ethernet12          -                   down                            1000         9100
+#Ethernet16          -                   down                            100000       9100
+#
+# show running-configuration interface Ethernet 8
+#!
+#interface Ethernet8
+# mtu 9100
+# speed 100000
+# shutdown
+#
+- name: Configure interfaces
+  sonic_interfaces:
+    config:
+      - name: Ethernet8
+        fec: FEC_AUTO
+      - name: Ethernet12
+        description: 'Ethernet Twelve'
+        mtu: 3500
+        enable: True
+        auto_negotiate: True
+      - name: Ethernet16
+        description: 'Ethernet Sixteen'
+        mtu: 3000
+        enable: False
+        speed: SPEED_40GB
+    state: overridden
+#
+# After state:
+# ------------
+#
+# show interface status | no-more
+#------------------------------------------------------------------------------------------
+#Name                Description         Admin     Oper      AutoNeg     Speed        MTU
+#------------------------------------------------------------------------------------------
+#Ethernet0           -                   up                              100000       9100
+#Ethernet4           -                   up                              100000       9100
+#Ethernet8           -                   up                              100000       9100
+#Ethernet12          Ethernet Twelve     up                  on          -            3500
+#Ethernet16          Ethernet Sixteen    down                            40000        3000
+#
+# show running-configuration interface Ethernet 8
+#!
+#interface Ethernet8
+# mtu 9100
+# speed 100000
+# fec AUTO
+# shutdown
+#
+# Using replaced
+#
+# Before state:
+# -------------
+#
+# show interface status | no-more
+#------------------------------------------------------------------------------------------
+#Name                Description         Admin     Oper      AutoNeg     Speed        MTU
+#------------------------------------------------------------------------------------------
+#Ethernet0           -                   up                              100000       9100
+#Ethernet4           -                   up                              100000       9100
+#Ethernet8           -                   down                            100000       9100
+#Ethernet12          -                   down                            1000         9100
+#Ethernet16          -                   down                            100000       9100
+#
+# show running-configuration interface Ethernet 8
+#!
+#interface Ethernet8
+# mtu 9100
+# speed 100000
+# shutdown
+#
+- name: Configure interfaces
+  sonic_interfaces:
+    config:
+      - name: Ethernet8
+        fec: FEC_AUTO
+      - name: Ethernet12
+        description: 'Ethernet Twelve'
+        mtu: 3500
+        enable: True
+        auto_negotiate: True
+      - name: Ethernet16
+        description: 'Ethernet Sixteen'
+        mtu: 3000
+        enable: False
+        speed: SPEED_40GB
+    state: replaced
+#
+# After state:
+# ------------
+#
+# show interface status | no-more
+#------------------------------------------------------------------------------------------
+#Name                Description         Admin     Oper      AutoNeg     Speed        MTU
+#------------------------------------------------------------------------------------------
+#Ethernet0           -                   up                              100000       9100
+#Ethernet4           -                   up                              100000       9100
+#Ethernet8           -                   down                            100000       9100
+#Ethernet12          Ethernet Twelve     up                  on          -            3500
+#Ethernet16          Ethernet Sixteen    down                            40000        3000
+#
+# show running-configuration interface Ethernet 8
+#!
+#interface Ethernet8
+# mtu 9100
+# speed 100000
+# fec AUTO
+# shutdown
 #
 """
 RETURN = """
