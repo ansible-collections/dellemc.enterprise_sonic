75a0e862653ad14cbfc40ee54bc26107eeacd5f4
kannankvs <kannan_kvs@dell.com>
2023-03-13 17:09:56 +0000

    bgp_af_ut: Fix3: idempotency test case modified to merged test case

--- tests/unit/modules/network/sonic/test_sonic_bgp_af.py
+++ tests/unit/modules/network/sonic/test_sonic_bgp_af.py
@@ -62,12 +62,12 @@ class TestSonicBgpModule(TestSonicModule):
 
         result = self.execute_module(changed=True)
         self.validate_config_requests()
-    def test_sonic_bgp_af_idemp_01(self):
-        set_module_args(self.fixture_data['idemp_01']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['idemp_01']['existing_bgp_config'])
-        self.initialize_config_requests(self.fixture_data['idemp_01']['expected_config_requests'])
+    def test_sonic_bgp_af_merged_03(self):
+        set_module_args(self.fixture_data['merged_03']['module_args'])
+        self.initialize_facts_get_requests(self.fixture_data['merged_03']['existing_bgp_config'])
+        self.initialize_config_requests(self.fixture_data['merged_03']['expected_config_requests'])
 
-        result = self.execute_module(changed=False)
+        result = self.execute_module(changed=True)
         self.validate_config_requests()
     def test_sonic_bgp_af_deleted_01(self):
         set_module_args(self.fixture_data['deleted_01']['module_args'])
