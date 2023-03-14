e13b8a4fef136eab343f54fd2fe7b4d4d71d184f
kannankvs <kannan_kvs@dell.com>
2023-03-14 11:16:27 +0000

    bgp_ext_communities_ut: Fix1: Added mandatory empty ext commmunity set in merged test case; deleted commented lines

--- tests/unit/modules/network/sonic/test_sonic_bgp_ext_communities.py
+++ tests/unit/modules/network/sonic/test_sonic_bgp_ext_communities.py
@@ -51,7 +51,6 @@ class TestSonicBgpModule(TestSonicModule):
         set_module_args(self.fixture_data['merged_01']['module_args'])
         self.initialize_facts_get_requests(self.fixture_data['merged_01']['existing_bgp_config'])
         self.initialize_config_requests(self.fixture_data['merged_01']['expected_config_requests'])
-
         result = self.execute_module(changed=True)
         self.validate_config_requests()
 
@@ -59,83 +58,75 @@ class TestSonicBgpModule(TestSonicModule):
         set_module_args(self.fixture_data['merged_02']['module_args'])
         self.initialize_facts_get_requests(self.fixture_data['merged_02']['existing_bgp_config'])
         self.initialize_config_requests(self.fixture_data['merged_02']['expected_config_requests'])
-
         result = self.execute_module(changed=True)
         self.validate_config_requests()
+
     def test_sonic_bgp_ext_communities_merged_03(self):
         set_module_args(self.fixture_data['merged_03']['module_args'])
         self.initialize_facts_get_requests(self.fixture_data['merged_03']['existing_bgp_config'])
         self.initialize_config_requests(self.fixture_data['merged_03']['expected_config_requests'])
-
         result = self.execute_module(changed=True)
         self.validate_config_requests()
-#    def test_sonic_bgp_ext_communities_idemp_01(self):
-#        set_module_args(self.fixture_data['idemp_01']['module_args'])
-#        self.initialize_facts_get_requests(self.fixture_data['idemp_01']['existing_bgp_config'])
-#        self.initialize_config_requests(self.fixture_data['idemp_01']['expected_config_requests'])
-#
-#        result = self.execute_module(changed=False)
-#        self.validate_config_requests()
+
     def test_sonic_bgp_ext_communities_deleted_01(self):
         set_module_args(self.fixture_data['deleted_01']['module_args'])
         self.initialize_facts_get_requests(self.fixture_data['deleted_01']['existing_bgp_config'])
         self.initialize_config_requests(self.fixture_data['deleted_01']['expected_config_requests'])
-
         result = self.execute_module(changed=True)
         self.validate_config_requests()
+
     def test_sonic_bgp_ext_communities_deleted_02(self):
         set_module_args(self.fixture_data['deleted_02']['module_args'])
         self.initialize_facts_get_requests(self.fixture_data['deleted_02']['existing_bgp_config'])
         self.initialize_config_requests(self.fixture_data['deleted_02']['expected_config_requests'])
-
         result = self.execute_module(changed=True)
         self.validate_config_requests()
+
     def test_sonic_bgp_ext_communities_deleted_03(self):
         set_module_args(self.fixture_data['deleted_03']['module_args'])
         self.initialize_facts_get_requests(self.fixture_data['deleted_03']['existing_bgp_config'])
         self.initialize_config_requests(self.fixture_data['deleted_03']['expected_config_requests'])
-
         result = self.execute_module(changed=True)
         self.validate_config_requests()
+
     def test_sonic_bgp_ext_communities_deleted_04(self):
         set_module_args(self.fixture_data['deleted_04']['module_args'])
         self.initialize_facts_get_requests(self.fixture_data['deleted_04']['existing_bgp_config'])
         self.initialize_config_requests(self.fixture_data['deleted_04']['expected_config_requests'])
-
         result = self.execute_module(changed=True)
         self.validate_config_requests()
+
     def test_sonic_bgp_ext_communities_deleted_05(self):
         set_module_args(self.fixture_data['deleted_05']['module_args'])
         self.initialize_facts_get_requests(self.fixture_data['deleted_05']['existing_bgp_config'])
         self.initialize_config_requests(self.fixture_data['deleted_05']['expected_config_requests'])
-
         result = self.execute_module(changed=True)
         self.validate_config_requests()
+
     def test_sonic_bgp_ext_communities_deleted_06(self):
         set_module_args(self.fixture_data['deleted_06']['module_args'])
         self.initialize_facts_get_requests(self.fixture_data['deleted_06']['existing_bgp_config'])
         self.initialize_config_requests(self.fixture_data['deleted_06']['expected_config_requests'])
-
         result = self.execute_module(changed=True)
         self.validate_config_requests()
+
     def test_sonic_bgp_ext_communities_deleted_07(self):
         set_module_args(self.fixture_data['deleted_07']['module_args'])
         self.initialize_facts_get_requests(self.fixture_data['deleted_07']['existing_bgp_config'])
         self.initialize_config_requests(self.fixture_data['deleted_07']['expected_config_requests'])
-
         result = self.execute_module(changed=True)
         self.validate_config_requests()
+
     def test_sonic_bgp_ext_communities_deleted_08(self):
         set_module_args(self.fixture_data['deleted_08']['module_args'])
         self.initialize_facts_get_requests(self.fixture_data['deleted_08']['existing_bgp_config'])
         self.initialize_config_requests(self.fixture_data['deleted_08']['expected_config_requests'])
-
         result = self.execute_module(changed=True)
         self.validate_config_requests()
+
     def test_sonic_bgp_ext_communities_deleted_09(self):
         set_module_args(self.fixture_data['deleted_09']['module_args'])
         self.initialize_facts_get_requests(self.fixture_data['deleted_09']['existing_bgp_config'])
         self.initialize_config_requests(self.fixture_data['deleted_09']['expected_config_requests'])
-
         result = self.execute_module(changed=True)
         self.validate_config_requests()
