168dd6948aee10ff6da3804f1b575ca8a76efbcd
kannankvs <kannan_kvs@dell.com>
2023-04-21 16:09:38 +0000

    Added few combinations to increase ceoverage percentage without replaced and overridden

--- tests/unit/modules/network/sonic/test_sonic_l2_interfaces.py
+++ tests/unit/modules/network/sonic/test_sonic_l2_interfaces.py
@@ -74,17 +74,3 @@ class TestSonicInterfacesModule(TestSonicModule):
         self.initialize_config_requests(self.fixture_data['deleted_02']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
-
-    def test_sonic_l2_interfaces_replaced_01(self):
-        set_module_args(self.fixture_data['replaced_01']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['replaced_01']['existing_l2_interfaces_config'])
-        self.initialize_config_requests(self.fixture_data['replaced_01']['expected_config_requests'])
-        result = self.execute_module(changed=True)
-        self.validate_config_requests()
-
-    def test_sonic_l2_interfaces_overridden_01(self):
-        set_module_args(self.fixture_data['overridden_01']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['overridden_01']['existing_l2_interfaces_config'])
-        self.initialize_config_requests(self.fixture_data['overridden_01']['expected_config_requests'])
-        result = self.execute_module(changed=True)
-        self.validate_config_requests()
