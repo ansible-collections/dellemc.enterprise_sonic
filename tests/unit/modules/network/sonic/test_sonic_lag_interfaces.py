091dabecff1471c88399da4b94682874ef4dcc34
kannankvs <kannan_kvs@dell.com>
2023-04-21 17:24:19 +0000

    added few combinations to increase coverage percentage without replaced and overridden

--- tests/unit/modules/network/sonic/test_sonic_lag_interfaces.py
+++ tests/unit/modules/network/sonic/test_sonic_lag_interfaces.py
@@ -68,16 +68,9 @@ class TestSonicInterfacesModule(TestSonicModule):
         result = self.execute_module(changed=True)
         self.validate_config_requests()
 
-    def test_sonic_lag_interfaces_replaced_01(self):
-        set_module_args(self.fixture_data['replaced_01']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['replaced_01']['existing_lag_interfaces_config'])
-        self.initialize_config_requests(self.fixture_data['replaced_01']['expected_config_requests'])
-        result = self.execute_module(changed=True)
-        self.validate_config_requests()
-
-    def test_sonic_lag_interfaces_overridden_01(self):
-        set_module_args(self.fixture_data['overridden_01']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['overridden_01']['existing_lag_interfaces_config'])
-        self.initialize_config_requests(self.fixture_data['overridden_01']['expected_config_requests'])
+    def test_sonic_lag_interfaces_deleted_03(self):
+        set_module_args(self.fixture_data['deleted_03']['module_args'])
+        self.initialize_facts_get_requests(self.fixture_data['deleted_03']['existing_lag_interfaces_config'])
+        self.initialize_config_requests(self.fixture_data['deleted_03']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
