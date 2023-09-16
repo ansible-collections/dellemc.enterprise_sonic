c1d1c5f18a3a91a7240bf4ef6d85e523ee9a422f
Shade Talabi <shade.talabi@dell.com>
2023-09-15 21:18:58 -0700

    Updated test cases and replaced coded

--- plugins/module_utils/network/sonic/config/stp/stp.py
+++ plugins/module_utils/network/sonic/config/stp/stp.py
@@ -1259,6 +1259,35 @@ class Stp(ConfigBase):
                     if ((hello_time and hello_time != cfg_hello_time) or (max_age and max_age != cfg_max_age) or
                             (fwd_delay and fwd_delay != cfg_fwd_delay) or (bridge_priority and bridge_priority != cfg_bridge_priority)):
                         vlans_list.append(cfg_vlan)
+
+                        if cfg_hello_time:
+                            if protocol == 'pvst':
+                                requests.append(self.get_delete_pvst_vlan_cfg_attr(cfg_vlan_id, 'hello-time'))
+                            elif protocol == 'rapid_pvst':
+                                requests.append(self.get_delete_rapid_pvst_vlan_cfg_attr(cfg_vlan_id, 'hello-time'))
+                        if cfg_max_age:
+                            if protocol == 'pvst':
+                                requests.append(self.get_delete_pvst_vlan_cfg_attr(cfg_vlan_id, 'max-age'))
+                            elif protocol == 'rapid_pvst':
+                                requests.append(self.get_delete_rapid_pvst_vlan_cfg_attr(cfg_vlan_id, 'max-age'))
+                        if cfg_fwd_delay:
+                            if protocol == 'pvst':
+                                requests.append(self.get_delete_pvst_vlan_cfg_attr(cfg_vlan_id, 'forwarding-delay'))
+                            elif protocol == 'rapid_pvst':
+                                requests.append(self.get_delete_rapid_pvst_vlan_cfg_attr(cfg_vlan_id, 'forwarding-delay'))
+                        if cfg_bridge_priority:
+                            if protocol == 'pvst':
+                                requests.append(self.get_delete_pvst_vlan_cfg_attr(cfg_vlan_id, 'bridge-priority'))
+                            elif protocol == 'rapid_pvst':
+                                requests.append(self.get_delete_rapid_pvst_vlan_cfg_attr(cfg_vlan_id, 'bridge-priority'))
+                        if cfg_interfaces:
+                            for cfg_intf in cfg_interfaces:
+                                cfg_intf_name = cfg_intf.get('intf_name', None)
+                                if protocol == 'pvst':
+                                    requests.append(self.get_delete_pvst_intf(cfg_vlan_id, cfg_intf_name))
+                                elif protocol == 'rapid_pvst':
+                                    requests.append(self.get_delete_rapid_pvst_intf(cfg_vlan_id, cfg_intf_name))
+
                     else:
                         if interfaces and cfg_interfaces:
                             intf_list = []
