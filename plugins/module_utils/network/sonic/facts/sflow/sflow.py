1a244596904b23daf0ed176473d8b19ccf3a5255
xhan-dell <Xiao.Han2@Dell.com>
2023-10-09 10:45:53 -0700

    cleaning up logging

--- plugins/module_utils/network/sonic/facts/sflow/sflow.py
+++ plugins/module_utils/network/sonic/facts/sflow/sflow.py
@@ -21,17 +21,6 @@ from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.s
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.sonic \
     import to_request, edit_config
 
-import logging
-facts_logger = logging.getLogger("sflow Facts")
-# This decides what is printed vv Only logging with higher priority gets printed. set to all caps level name
-facts_logger.setLevel(logging.DEBUG)
-# if not facts_logger.handlers:
-#     # check just in case to prevent double calls from creating double output
-#     logger_handler = logging.FileHandler('myLogs.txt', mode='a+', encoding='utf-8')
-#     logging_format = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', '%Y-%m-%d %H:%M:%S', style='{')
-#     logger_handler.setFormatter(logging_format)
-#     facts_logger.addHandler(logger_handler)
-
 class SflowFacts(object):
     """ The sonic sflow fact class
     """
@@ -61,18 +50,14 @@ class SflowFacts(object):
 
         if not data:
             data = self.get_sflow_info()
-            facts_logger.debug(f"returned from device data is <{data}>")
         
         data = self.format_to_argspec(data)
-        facts_logger.debug(f"data formatted to argspec is <{data}>")
 
         #validate can add null values for things missing from device config, 
         #   so doing that before remove empties
         cleaned_data = utils.remove_empties(
             utils.validate_config(self.argument_spec, data)
         )
-
-        facts_logger.debug(f"cleaned up and validated data is <{cleaned_data}>")
 
         ansible_facts['ansible_network_resources'].pop('sflow', None)
         if cleaned_data:
@@ -132,7 +117,6 @@ class SflowFacts(object):
         except ConnectionError as exc:
             self._module.fail_json(msg = str(exc))
 
-        facts_logger.debug(f"REST response to get facts is <{response}>")
         response_body = {}
         try:
             response_body = response[0][1][response_key]
