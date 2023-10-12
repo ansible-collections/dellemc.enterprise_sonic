e2883f97a8b0a918e53958e297bab83e35a5c697
xhan-dell <Xiao.Han2@Dell.com>
2023-10-12 13:30:32 -0700

    rename missed name fixes

--- tests/unit/modules/network/sonic/test_sonic_sflow.py
+++ tests/unit/modules/network/sonic/test_sonic_sflow.py
@@ -45,88 +45,88 @@ class TestSonicInterfacesModule(TestSonicModule):
         self.mock_config_edit_config.stop()
         self.mock_utils_edit_config.stop()
 
-    def test_sonic_static_routes_merged_01(self):
+    def test_sonic_sflow_merged_01(self):
         set_module_args(self.fixture_data['merged_01']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['merged_01']['existing_static_routes_config'])
+        self.initialize_facts_get_requests(self.fixture_data['merged_01']['existing_sflow_config'])
         self.initialize_config_requests(self.fixture_data['merged_01']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
 
-    def test_sonic_static_routes_merged_02(self):
+    def test_sonic_sflow_merged_02(self):
         set_module_args(self.fixture_data['merged_02']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['merged_02']['existing_static_routes_config'])
+        self.initialize_facts_get_requests(self.fixture_data['merged_02']['existing_sflow_config'])
         result = self.execute_module(changed=False)
         self.validate_config_requests()
 
-    def test_sonic_static_routes_merged_03(self):
+    def test_sonic_sflow_merged_03(self):
         set_module_args(self.fixture_data['merged_03']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['merged_03']['existing_static_routes_config'])
+        self.initialize_facts_get_requests(self.fixture_data['merged_03']['existing_sflow_config'])
         result = self.execute_module(changed=False)
         self.validate_config_requests()
 
-    def test_sonic_static_routes_deleted_01(self):
+    def test_sonic_sflow_deleted_01(self):
         set_module_args(self.fixture_data['deleted_01']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['deleted_01']['existing_static_routes_config'])
+        self.initialize_facts_get_requests(self.fixture_data['deleted_01']['existing_sflow_config'])
         self.initialize_config_requests(self.fixture_data['deleted_01']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
 
-    def test_sonic_static_routes_deleted_02(self):
+    def test_sonic_sflow_deleted_02(self):
         set_module_args(self.fixture_data['deleted_02']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['deleted_02']['existing_static_routes_config'])
+        self.initialize_facts_get_requests(self.fixture_data['deleted_02']['existing_sflow_config'])
         result = self.execute_module(changed=False)
         self.validate_config_requests()
 
-    def test_sonic_static_routes_deleted_03(self):
+    def test_sonic_sflow_deleted_03(self):
         set_module_args(self.fixture_data['deleted_03']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['deleted_03']['existing_static_routes_config'])
+        self.initialize_facts_get_requests(self.fixture_data['deleted_03']['existing_sflow_config'])
         self.initialize_config_requests(self.fixture_data['deleted_03']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
 
-    def test_sonic_static_routes_deleted_04(self):
+    def test_sonic_sflow_deleted_04(self):
         set_module_args(self.fixture_data['deleted_04']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['deleted_04']['existing_static_routes_config'])
+        self.initialize_facts_get_requests(self.fixture_data['deleted_04']['existing_sflow_config'])
         self.initialize_config_requests(self.fixture_data['deleted_04']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
 
-    def test_sonic_static_routes_deleted_05(self):
+    def test_sonic_sflow_deleted_05(self):
         set_module_args(self.fixture_data['deleted_05']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['deleted_05']['existing_static_routes_config'])
+        self.initialize_facts_get_requests(self.fixture_data['deleted_05']['existing_sflow_config'])
         result = self.execute_module(changed=False)
         self.validate_config_requests()
 
-    def test_sonic_static_routes_deleted_06(self):
+    def test_sonic_sflow_deleted_06(self):
         set_module_args(self.fixture_data['deleted_06']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['deleted_06']['existing_static_routes_config'])
+        self.initialize_facts_get_requests(self.fixture_data['deleted_06']['existing_sflow_config'])
         self.initialize_config_requests(self.fixture_data['deleted_06']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
 
-    def test_sonic_static_routes_overridden_01(self):
+    def test_sonic_sflow_overridden_01(self):
         set_module_args(self.fixture_data['overridden_01']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['overridden_01']['existing_static_routes_config'])
+        self.initialize_facts_get_requests(self.fixture_data['overridden_01']['existing_sflow_config'])
         self.initialize_config_requests(self.fixture_data['overridden_01']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
 
-    def test_sonic_static_routes_overridden_02(self):
+    def test_sonic_sflow_overridden_02(self):
         set_module_args(self.fixture_data['overridden_02']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['overridden_02']['existing_static_routes_config'])
+        self.initialize_facts_get_requests(self.fixture_data['overridden_02']['existing_sflow_config'])
         self.initialize_config_requests(self.fixture_data['overridden_02']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
 
-    def test_sonic_static_routes_replaced_01(self):
+    def test_sonic_sflow_replaced_01(self):
         set_module_args(self.fixture_data['replaced_01']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['replaced_01']['existing_static_routes_config'])
+        self.initialize_facts_get_requests(self.fixture_data['replaced_01']['existing_sflow_config'])
         result = self.execute_module(changed=False)
         self.validate_config_requests()
 
-    def test_sonic_static_routes_replaced_02(self):
+    def test_sonic_sflow_replaced_02(self):
         set_module_args(self.fixture_data['replaced_02']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['replaced_02']['existing_static_routes_config'])
+        self.initialize_facts_get_requests(self.fixture_data['replaced_02']['existing_sflow_config'])
         self.initialize_config_requests(self.fixture_data['replaced_02']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
