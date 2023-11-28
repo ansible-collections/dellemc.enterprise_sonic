c68746ef95f539b8e218fcac194ffd3bf2491cc3
mingjun-ansible <mingjun_zhang@dell.com>
2023-11-28 09:57:28 -0800

    Add more UT cases

--- tests/unit/modules/network/sonic/test_sonic_static_routes.py
+++ tests/unit/modules/network/sonic/test_sonic_static_routes.py
@@ -71,3 +71,17 @@ class TestSonicInterfacesModule(TestSonicModule):
         self.initialize_config_requests(self.fixture_data['deleted_02']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
+
+    def test_sonic_static_routes_replaced_1(self):
+        set_module_args(self.fixture_data['replaced_01']['module_args'])
+        self.initialize_facts_get_requests(self.fixture_data['replaced_01']['existing_static_routes_config'])
+        self.initialize_config_requests(self.fixture_data['replaced_01']['expected_config_requests'])
+        result = self.execute_module(changed=True)
+        self.validate_config_requests()
+
+    def test_sonic_static_routes_overridden_01(self):
+        set_module_args(self.fixture_data['overridden_01']['module_args'])
+        self.initialize_facts_get_requests(self.fixture_data['overridden_01']['existing_static_routes_config'])
+        self.initialize_config_requests(self.fixture_data['overridden_01']['expected_config_requests'])
+        result = self.execute_module(changed=True)
+        self.validate_config_requests()
