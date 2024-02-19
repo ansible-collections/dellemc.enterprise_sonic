d4ff83b72dddb23694efe0ab870f59355a1d988b
xhan-dell <Xiao.Han2@Dell.com>
2024-02-19 19:28:00 +0000

    use unique unit test class name

--- tests/unit/modules/network/sonic/test_sonic_sflow.py
+++ tests/unit/modules/network/sonic/test_sonic_sflow.py
@@ -14,7 +14,7 @@ from ansible_collections.dellemc.enterprise_sonic.tests.unit.modules.utils impor
 from .sonic_module import TestSonicModule
 
 
-class TestSonicInterfacesModule(TestSonicModule):
+class TestSonicSflowModule(TestSonicModule):
     module = sonic_sflow
 
     @classmethod
@@ -32,7 +32,7 @@ class TestSonicInterfacesModule(TestSonicModule):
         cls.fixture_data = cls.load_fixtures('sonic_sflow.yaml')
 
     def setUp(self):
-        super(TestSonicInterfacesModule, self).setUp()
+        super(TestSonicSflowModule, self).setUp()
         self.facts_edit_config = self.mock_facts_edit_config.start()
         self.facts_edit_config.side_effect = self.facts_side_effect
 
@@ -43,7 +43,7 @@ class TestSonicInterfacesModule(TestSonicModule):
         self.get_interface_naming_mode.return_value = 'native'
 
     def tearDown(self):
-        super(TestSonicInterfacesModule, self).tearDown()
+        super(TestSonicSflowModule, self).tearDown()
         self.mock_facts_edit_config.stop()
         self.mock_config_edit_config.stop()
         self.mock_get_interface_naming_mode.stop()
