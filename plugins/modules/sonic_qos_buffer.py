789c4812faf40c518149579fd21772b806a8fe56
Shade Talabi <shade.talabi@dell.com>
2024-02-22 12:23:51 -0800

    Corrected indentation

--- plugins/modules/sonic_qos_buffer.py
+++ plugins/modules/sonic_qos_buffer.py
@@ -124,25 +124,25 @@ EXAMPLES = """
 
   - name: Merge QoS buffer configuration
     dellemc.enterprise_sonic.sonic_qos_buffer:
-    config:
-      buffer_pools:
-        - name: ingress_lossless_pool
-          xoff: 3500000
-      buffer_profiles:
-        - name: profile1
-          pool: ingress_lossless_pool
-          size: 45
-          static_threshold: 25
-          pause_threshold: 55000
-        - name: profile2
-          pool: egress_lossless_pool
-          size: 85
-          dynamic_threshold: -2
-        - name: profile3
-          pool: egress_lossy_pool
-          size: 90
-          static_threshold: 30
-    state: merged
+      config:
+        buffer_pools:
+          - name: ingress_lossless_pool
+            xoff: 3500000
+        buffer_profiles:
+          - name: profile1
+            pool: ingress_lossless_pool
+            size: 45
+            static_threshold: 25
+            pause_threshold: 55000
+          - name: profile2
+            pool: egress_lossless_pool
+            size: 85
+            dynamic_threshold: -2
+          - name: profile3
+            pool: egress_lossy_pool
+            size: 90
+            static_threshold: 30
+      state: merged
 
 # After state:
 # ------------
@@ -169,15 +169,15 @@ EXAMPLES = """
 
   - name: Delete QoS buffer profile configuration
     dellemc.enterprise_sonic.sonic_qos_buffer:
-    config:
-      buffer_profiles:
-        - name: profile1
-          static_threshold: 25
-          pause_threshold: 55000
-        - name: profile2
-          dynamic_threshold: -2
-        - name: profile3
-    state: deleted
+      config:
+        buffer_profiles:
+          - name: profile1
+            static_threshold: 25
+            pause_threshold: 55000
+          - name: profile2
+            dynamic_threshold: -2
+          - name: profile3
+      state: deleted
 
 # After state:
 # ------------
