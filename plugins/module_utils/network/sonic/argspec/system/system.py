96ca2c445659f104075e64a412cffcdc3d1aa572
SaiTeja0510 <saiteja.kolar1805@gmail.com>
2024-02-22 15:27:41 +0530

    Auto-breakout - Module implementation

--- plugins/module_utils/network/sonic/argspec/system/system.py
+++ plugins/module_utils/network/sonic/argspec/system/system.py
@@ -52,7 +52,8 @@ class SystemArgs(object):  # pylint: disable=R0903
                 'interface_naming': {
                     'choices': ['standard', 'native'],
                     'type': 'str'
-                }
+                },
+                'auto_breakout': {'type': 'str'}
             },
             'type': 'dict'
         },
