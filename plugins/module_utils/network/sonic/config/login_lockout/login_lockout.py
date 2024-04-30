ee32fcf45736f59021904d578caebddf3c1a793a
arulkumar9690 <63288235+arulkumar9690@users.noreply.github.com>
2024-04-30 12:15:14 +0530

    Update login_lockout.py
    - Added support for overridden and replaced cases

--- plugins/module_utils/network/sonic/config/login_lockout/login_lockout.py
+++ plugins/module_utils/network/sonic/config/login_lockout/login_lockout.py
@@ -3,35 +3,37 @@
 # Copyright 2023 Dell Inc. or its subsidiaries. All Rights Reserved
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-"""
+""" 
 The sonic_login_lockout class
 It is in this file where the current configuration (as dict)
 is compared to the provided configuration (as dict) and the command set
 necessary to bring the current configuration to it's desired end-state is
-created
-"""
-
+created 
+""" 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.cfg.base import (
     ConfigBase,
-)
+)   
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.utils import (
     to_list,
 )
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.facts import Facts
-
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.utils.utils import (
     get_diff,
-    update_states
+    update_states,
+    remove_empties_from_list 
 )
-
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.sonic import (
-    to_request,
+    to_request, 
     edit_config
 )
-
+from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.utils.formatted_diff_utils import (
+    __DELETE_SUBCONFIG_AND_LEAFS,
+    get_formatted_config_diff,
+    get_new_config
+)
 from ansible.module_utils.connection import ConnectionError
 
 PATCH = 'patch'
@@ -80,9 +82,11 @@ class Login_lockout(ConfigBase):
         :returns: The result from module execution
         """
         result = {'changed': False}
-        warnings = []
+        warnings = list()
+        commands = list()
 
         existing_login_lockout_facts = self.get_login_lockout_facts()
+
         commands, requests = self.set_config(existing_login_lockout_facts)
         if commands:
             if not self._module.check_mode:
@@ -91,15 +95,16 @@ class Login_lockout(ConfigBase):
                 except ConnectionError as exc:
                     self._module.fail_json(msg=str(exc), code=exc.code)
             result['changed'] = True
+        result['commands'] = commands
 
         changed_login_lockout_facts = self.get_login_lockout_facts()
 
         result['before'] = existing_login_lockout_facts
+
         if result['changed']:
             result['after'] = changed_login_lockout_facts
-
-        result['commands'] = commands
         result['warnings'] = warnings
+
         return result
 
     def set_config(self, existing_login_lockout_facts):
@@ -111,8 +116,11 @@ class Login_lockout(ConfigBase):
                   to the desired configuration
         """
         want = self._module.params['config']
+
         have = existing_login_lockout_facts
+
         resp = self.set_state(want, have)
+
         return to_list(resp)
 
     def set_state(self, want, have):
@@ -130,28 +138,59 @@ class Login_lockout(ConfigBase):
             commands, requests = self._state_deleted(want, have, diff)
         elif state == 'merged':
             commands, requests = self._state_merged(diff)
+        elif state == 'replaced':
+            commands, requests = self._state_replaced_overridden(want, have)
+        elif state == 'overridden':
+            commands, requests = self._state_replaced_overridden(want, have)
+        return commands, requests
+
+
+    def _state_replaced_overridden(self, want, have):
+        """ The command generator when state is replaced or overridden
+
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        commands = []
+        requests = []
+        state = self._module.params['state']
+
+        if have:
+            commands = have
+            requests.extend(self.get_delete_login_lockout_completely_requests(commands))
+
+        if want:
+            commands = want
+            requests.extend(self.get_modify_specific_login_lockout_param_requests(commands))
+
+        if (state == 'overridden'):
+            commands = update_states(commands, 'overridden')
+        else:
+            commands = update_states(commands, 'replaced')
+
         return commands, requests
 
     def _state_merged(self, diff):
         """ The command generator when state is merged
-
         :rtype: A list
         :returns: the commands necessary to merge the provided into
                   the current configuration
         """
-        commands = diff
+        commands = []
         requests = []
-        requests.extend(self.get_modify_specific_login_lockout_param_requests(commands))
-        if commands and len(requests) > 0:
-            commands = update_states(commands, 'merged')
-        else:
-            commands = []
+
+        commands = diff
+
+        if diff:
+             requests = self.get_modify_specific_login_lockout_param_requests(diff)
+             if len(requests) > 0:
+                 commands = update_states(commands, 'merged')
 
         return commands, requests
 
     def _state_deleted(self, want, have, diff):
         """ The command generator when state is deleted
-
         :rtype: A list
         :returns: the commands necessary to remove the current configuration
                   of the provided objects
@@ -178,24 +217,27 @@ class Login_lockout(ConfigBase):
         """Get requests to modify specific Login Lockout configurations
         based on the command specified
         """
+
         requests = []
+
+        config_dict = {}
 
         if not command:
             return requests
+
         if 'console-exempt' in command and command['console-exempt'] is not None:
-            payload = {'openconfig-system-ext:console-exempt': command['console-exempt']}
-            url = self.login_lockout_config_path['console-exempt']
-            requests.append({'path': url, 'method': PATCH, 'data': payload})
+            config_dict['console-exempt'] = command['console-exempt']
 
         if 'period' in command and command['period'] is not None:
-            payload = {'openconfig-system-ext:lockout-period': int(command['period'])}
-            url = self.login_lockout_config_path['period']
-            requests.append({'path': url, 'method': PATCH, 'data': payload})
+            config_dict['lockout-period'] = int(command['period'])
 
         if 'max-retries' in command and command['max-retries'] is not None:
-            payload = {'openconfig-system-ext:max-retry': int(command['max-retries'])}
-            url = self.login_lockout_config_path['max-retries']
-            requests.append({'path': url, 'method': PATCH, 'data': payload})
+            config_dict['max-retry'] = int(command['max-retries'])
+
+        payload = { "openconfig-system-ext:config" : config_dict }
+
+        requests.append({'path': self.login_lockout_path, 'method': PATCH, 'data' : payload})
+
         return requests
 
     def get_delete_login_lockout_completely_requests(self, have):
