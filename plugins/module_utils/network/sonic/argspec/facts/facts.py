86d072b6f477cf192b72b05328633a6e685399b0
arulkumar9690 <63288235+arulkumar9690@users.noreply.github.com>
2024-08-08 11:37:17 +0530

    Update facts.py

--- plugins/module_utils/network/sonic/argspec/facts/facts.py
+++ plugins/module_utils/network/sonic/argspec/facts/facts.py
@@ -78,7 +78,8 @@ class FactsArgs(object):  # pylint: disable=R0903
         'pim_interfaces',
         'login_lockout',
         'poe',
-        'mgmt_servers'
+        'mgmt_servers',
+        'banner'
     ]
 
     argument_spec = {
