3c0a6e387242e038ddfc417ad5c14e52f71623fb
arulkumar9690 <63288235+arulkumar9690@users.noreply.github.com>
2024-08-08 17:41:20 +0530

    Update facts.py

--- plugins/module_utils/network/sonic/facts/facts.py
+++ plugins/module_utils/network/sonic/facts/facts.py
@@ -78,7 +78,9 @@ from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.s
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.poe.poe import PoeFacts
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.mgmt_servers.mgmt_servers import Mgmt_serversFacts
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.ospf_area.ospf_area import Ospf_areaFacts
-from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.password_complexity.password_complexity import Password_complexityFacts
+from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.password_complexity.password_complexity import (
+    Password_complexityFacts,
+)
 
 
 FACT_LEGACY_SUBSETS = {}
