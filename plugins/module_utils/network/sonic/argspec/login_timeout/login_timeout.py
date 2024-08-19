d2833ee6f2dffdec3236aa7dd70b9bce513bbbb8
arulkumar9690 <63288235+arulkumar9690@users.noreply.github.com>
2024-08-19 14:52:56 +0530

    Update login_timeout.py

--- plugins/module_utils/network/sonic/argspec/login_timeout/login_timeout.py
+++ plugins/module_utils/network/sonic/argspec/login_timeout/login_timeout.py
@@ -29,6 +29,7 @@ The arg spec for the sonic_login_timeout module
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
+
 class Login_timeoutArgs(object):  # pylint: disable=R0903
     """The arg spec for the sonic_login_timeout module
     """
