9c499662189255acaee81788ab26f9de5cc176a0
Austin Whaley <austin_whaley@dell.com>
2024-11-26 19:28:25 -0500

    Updating interfaces.py to be pep8 and pylint compliant

--- plugins/module_utils/network/sonic/config/interfaces/interfaces.py
+++ plugins/module_utils/network/sonic/config/interfaces/interfaces.py
@@ -452,9 +452,9 @@ class Interfaces(ConfigBase):
             if attr == 'speed':
                 valid_intf_speeds = retrieve_valid_intf_speed(self._module, intf_name)
                 if self.is_port_in_port_group(intf_name) and (intf_speed_to_number_map.get(c_attr) not in valid_intf_speeds):
-                    self._module.fail_json(msg=("If the port-group is in the default speed you will be unable to set a member interface to a non-default speed. "
-                                           "Please use the port-group module to change the port-group of the member interface's speed before changing the interface's port speed. "
-                                           "Valid speeds for {} are currently {}").format(intf_name, valid_intf_speeds))
+                    self._module.fail_json(msg=("If a port-group is in the default speed you will be unable to set a member interface to a non-default speed. "
+                        "Please use the port-group module to change the port-group of the member interface's speed before changing the interface's port speed."
+                        " Valid speeds for {} are currently {}").format(intf_name, valid_intf_speeds))
                 payload['openconfig-if-ethernet:config'][payload_attr] = 'openconfig-if-ethernet:' + c_attr
             if attr == 'advertised_speed':
                 c_ads = c_attr if c_attr else []
