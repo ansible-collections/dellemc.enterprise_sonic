26cbf651b741772fc7c06d60232f6b651d1801c1
root <root@Oliver-VM.cec.delllabs.net>
2024-12-02 19:27:05 -0500

    fix sanity issue

--- plugins/module_utils/network/sonic/facts/qos_interfaces/qos_interfaces.py
+++ plugins/module_utils/network/sonic/facts/qos_interfaces/qos_interfaces.py
@@ -85,8 +85,8 @@ class Qos_interfacesFacts(object):
                     scheduler_policy = intf['output']['scheduler-policy']['config']['name']
                     config_dict['scheduler_policy'] = scheduler_policy
 
-                if ('openconfig-qos-buffer:cable-length' in intf and 
-                        'config' in intf['openconfig-qos-buffer:cable-length'] and 
+                if ('openconfig-qos-buffer:cable-length' in intf and
+                        'config' in intf['openconfig-qos-buffer:cable-length'] and
                         'length' in intf['openconfig-qos-buffer:cable-length']['config']):
                     cable_length = intf['openconfig-qos-buffer:cable-length']['config']['length']
                     config_dict['cable_length'] = cable_length
