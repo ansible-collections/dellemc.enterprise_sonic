2083e0abb2eb4cc4e0c107badc4298d633367622
aida-shumburo <aida.shumburo@dell.com>
2025-01-09 15:24:24 -0500

    updated facts

--- plugins/module_utils/network/sonic/facts/snmp/snmp.py
+++ plugins/module_utils/network/sonic/facts/snmp/snmp.py
@@ -44,8 +44,6 @@ class SnmpFacts(object):
         :rtype: dictionary
         :returns: facts
         """
-        if connection:  # just for linting purposes, remove
-            pass
 
         if not data:
             # Fetch data from the current device configuration
@@ -82,25 +80,80 @@ class SnmpFacts(object):
         except ConnectionError as exc:
             self._module.fail_json(msg=str(exc), code=exc.code)
         
-        snmp_list = []
+        snmp_dict = {}
+        snmp_configs = []
         if "sonic-snmp:sonic-snmp" in response[0][1]:
             snmp_list = response[0][1].get("sonic-snmp:sonic-snmp", {})
 
-        return snmp_list
+            snmp_dict.update(self.get_snmp_users(snmp_list.get("config")))
+            snmp_dict.update(self.get_snmp_hosts(snmp_list.get("config")))
+            snmp_dict.update(self.get_snmp_agentaddress(snmp_list.get("config")))
 
-    def fill_snmp_users(self, snmps, snmp_users):
-        """
-        Add snmp users to the snmp list
-        """
-        for snmp_user in snmp_users:
-            snmps.append(snmp_user)
+        if snmp_dict:
+            snmp_configs.append(snmp_dict)
 
-    def fill_snmp_hosts(self, snmps, snmp_hosts):
+        return snmp_configs
+
+    def get_snmp_users(self, snmp_list):
         """
-        Add snmp hosts to the snmp list
+        Get snmp users from the snmp list
         """
-        for snmp_host in snmp_hosts:
-            snmps.append(snmp_host)
+        user_dict = {}
+        user = snmp_list.get("user")
+
+        group = "SNMP_SERVER_GROUP/SNMP_SERVER_GROUP_LIST"
+        group_member_list = "SNMP_SERVER_GROUP_MEMBER/SNMP_SERVER_GROUP_MEMBER_LIST/"
+
+        user = "SNMP_SERVER_GROUP_USER/SNMP_SERVER_GROUP_USER_LIST"
+
+        if user:
+            self.update_dict(user_dict, "group", group_memmber_list.get("groupName"))
+            self.update_dict(user_dict, "name", group_member_list.get("securityName"))
+            self.update_dict(user_dict, "auth/auth_type", group_member_list.get(""))
+            self.update_dict(user_dict, "auth/key", group_member_list.get(""))
+            self.update_dict(user_dict, "priv/priv_type", group_member_list.get(""))
+            self.update_dict(user_dict, "priv/key", group_member_list.get(""))
+            #self.update_dict(user_dict, "encrypted", group_member_list.get("encrypted"))
+
+        return user_dict
+
+    def get_snmp_hosts(self, snmp_list):
+        """
+        Get snmp hosts from the snmp list
+        """
+        hosts_dict = {}
+        host = snmp_list.get("host")
+
+        server_params = "SNMP_SERVER_PARAMS/SNMP_SERVER_PARAMS_LIST"
+
+        server_target = "SNMP_SERVER_TARGET/SNMP_SERVER_TARGET_LIST"
+
+        if host:
+            self.update_dict(host_dict, "user/name", server_params.get("user"))
+            self.update_dict(host_dict, "user/security_level", server_params.get("security-level"))
+            self.update_dict(host_dict, "ip", server_target.get("ip"))
+            self.update_dict(host_dict, "retries", server_target.get("retries"))
+
+        return hosts_dict
+
+    def get_snmp_agentaddress(self, snmp_list):
+        """
+        Get snmp agent address from the snmp list
+        """
+        agentaddress_dict = {}
+        agentaddress = snmp_list.get("agentaddress")
+
+        agent_address_config = "SNMP_AGENT_ADDRESS_CONFIG/SNMP_AGENT_ADDRESS_CONFIG_LIST"
+
+        if agenaddress:
+            self.update_dict(agentaddress_dict, "interface", agent_address_config.get("interface"))
+            self.update_dict(agentaddress_dict, "ip", agent_address_config.get("ip"))
+            self.update_dict(agentaddress_dict, "name") # created
+            self.update_dict(agentaddress_dict, "port", agent_address_config.get("port"))
+
+        return agentaddress_dict
+
+
 
     def render_config(self, spec, conf):
         """
@@ -114,3 +167,10 @@ class SnmpFacts(object):
         """
         return conf
  
+     def update_dict(self, dict, key, value, parent_key=None):
+        if value not in [None, {}, [], ()]:
+            if parent_key:
+                dict.setdefault(parent_key, {})
+                dict[parent_key][key] = value
+            else:
+                dict[key] = value
