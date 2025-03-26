599b2aa521a59fe1bd7427af6ea0e08eb928339a
Mansi Jharia <mansi.jharia@dell.com>
2025-03-26 16:40:58 +0530

    updating ospfv3_interfaces ansible

--- tests/unit/modules/network/sonic/test_sonic_ospfv3_interfaces.py
+++ tests/unit/modules/network/sonic/test_sonic_ospfv3_interfaces.py
@@ -6,7 +6,7 @@ from ansible_collections.dellemc.enterprise_sonic.tests.unit.compat.mock import 
     patch,
 )
 from ansible_collections.dellemc.enterprise_sonic.plugins.modules import (
-    sonic_ospfv2_interfaces,
+    sonic_ospfv3_interfaces,
 )
 from ansible_collections.dellemc.enterprise_sonic.tests.unit.modules.utils import (
     set_module_args,
@@ -14,16 +14,16 @@ from ansible_collections.dellemc.enterprise_sonic.tests.unit.modules.utils impor
 from .sonic_module import TestSonicModule
 
 
-class TestSonicOspfv2InterfacesModule(TestSonicModule):
-    module = sonic_ospfv2_interfaces
+class TestSonicOspfv3InterfacesModule(TestSonicModule):
+    module = sonic_ospfv3_interfaces
 
     @classmethod
     def setUpClass(cls):
         cls.mock_facts_edit_config = patch(
-            "ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.ospfv2_interfaces.ospfv2_interfaces.edit_config"
+            "ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.ospfv3_interfaces.ospfv3_interfaces.edit_config"
         )
         cls.mock_config_edit_config = patch(
-            "ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.config.ospfv2_interfaces.ospfv2_interfaces.edit_config"
+            "ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.config.ospfv3_interfaces.ospfv3_interfaces.edit_config"
         )
         cls.mock_bgp_utils_edit_config = patch(
             "ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.utils.bgp_utils.edit_config"
@@ -31,10 +31,10 @@ class TestSonicOspfv2InterfacesModule(TestSonicModule):
         cls.mock_get_interface_naming_mode = patch(
             "ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.utils.utils.get_device_interface_naming_mode"
         )
-        cls.fixture_data = cls.load_fixtures('sonic_ospfv2_interfaces.yaml')
+        cls.fixture_data = cls.load_fixtures('sonic_ospfv3_interfaces.yaml')
 
     def setUp(self):
-        super(TestSonicOspfv2InterfacesModule, self).setUp()
+        super(TestSonicOspfv3InterfacesModule, self).setUp()
         self.facts_edit_config = self.mock_facts_edit_config.start()
         self.config_edit_config = self.mock_config_edit_config.start()
         self.utils_edit_config = self.mock_bgp_utils_edit_config.start()
@@ -47,50 +47,50 @@ class TestSonicOspfv2InterfacesModule(TestSonicModule):
         self.get_interface_naming_mode.return_value = 'standard'
 
     def tearDown(self):
-        super(TestSonicOspfv2InterfacesModule, self).tearDown()
+        super(TestSonicOspfv3InterfacesModule, self).tearDown()
         self.mock_facts_edit_config.stop()
         self.mock_config_edit_config.stop()
         self.mock_bgp_utils_edit_config.stop()
         self.mock_get_interface_naming_mode.stop()
 
-    def test_sonic_ospfv2_interfaces_merged_01(self):
+    def test_sonic_ospfv3_interfaces_merged_01(self):
         set_module_args(self.fixture_data['merged_01']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['merged_01']['existing_ospfv2_config'])
+        self.initialize_facts_get_requests(self.fixture_data['merged_01']['existing_ospfv3_config'])
         self.initialize_config_requests(self.fixture_data['merged_01']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
 
-    def test_sonic_ospfv2_interfaces_merged_02(self):
+    def test_sonic_ospfv3_interfaces_merged_02(self):
         set_module_args(self.fixture_data['merged_02']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['merged_02']['existing_ospfv2_config'])
+        self.initialize_facts_get_requests(self.fixture_data['merged_02']['existing_ospfv3_config'])
         self.initialize_config_requests(self.fixture_data['merged_02']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
 
-    def test_sonic_ospfv2_interfaces_deleted_01(self):
+    def test_sonic_ospfv3_interfaces_deleted_01(self):
         set_module_args(self.fixture_data['deleted_01']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['deleted_01']['existing_ospfv2_config'])
+        self.initialize_facts_get_requests(self.fixture_data['deleted_01']['existing_ospfv3_config'])
         self.initialize_config_requests(self.fixture_data['deleted_01']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
 
-    def test_sonic_ospfv2_interfaces_deleted_02(self):
+    def test_sonic_ospfv3_interfaces_deleted_02(self):
         set_module_args(self.fixture_data['deleted_02']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['deleted_02']['existing_ospfv2_config'])
+        self.initialize_facts_get_requests(self.fixture_data['deleted_02']['existing_ospfv3_config'])
         self.initialize_config_requests(self.fixture_data['deleted_02']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
 
-    def test_sonic_ospfv2_interfaces_replaced_01(self):
+    def test_sonic_ospfv3_interfaces_replaced_01(self):
         set_module_args(self.fixture_data['replaced_01']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['replaced_01']['existing_ospfv2_config'])
+        self.initialize_facts_get_requests(self.fixture_data['replaced_01']['existing_ospfv3_config'])
         self.initialize_config_requests(self.fixture_data['replaced_01']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
 
-    def test_sonic_ospfv2_interfaces_overridden_01(self):
+    def test_sonic_ospfv3_interfaces_overridden_01(self):
         set_module_args(self.fixture_data['overridden_01']['module_args'])
-        self.initialize_facts_get_requests(self.fixture_data['overridden_01']['existing_ospfv2_config'])
+        self.initialize_facts_get_requests(self.fixture_data['overridden_01']['existing_ospfv3_config'])
         self.initialize_config_requests(self.fixture_data['overridden_01']['expected_config_requests'])
         result = self.execute_module(changed=True)
         self.validate_config_requests()
