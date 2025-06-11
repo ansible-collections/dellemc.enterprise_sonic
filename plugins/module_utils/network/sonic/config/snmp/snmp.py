fe226b26544bea8dd4f9294d13871a177798435b
Aida Shumburo <aida.shumburo@dell.com>
2025-06-11 15:55:42 -0400

    update snmp UT test and remove unused import

--- plugins/module_utils/network/sonic/config/snmp/snmp.py
+++ plugins/module_utils/network/sonic/config/snmp/snmp.py
@@ -13,7 +13,6 @@ created
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
-from copy import deepcopy
 from ansible.module_utils.connection import ConnectionError
 
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.cfg.base import ConfigBase
