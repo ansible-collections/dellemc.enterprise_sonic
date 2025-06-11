6c3bea275838eb039036f428ad5135cc35023fb0
PraneshRaagavS <87803748+PraneshRaagavS@users.noreply.github.com>
2025-06-11 11:53:24 +0530

    Add Dell Enterprise Sonic_ptp_default_ds module (#547)
    * Add Dell Enterprise Sonic_ptp_default_ds module
    
    * syncing latest changes
    
    * addressing review comments
    
    * adding comments
    
    * Addressing review comments
    
    * addressing review comments
    
    * addressing review comments
    
    * addressing review comments
    
    * addressing review comments
    
    * addressing review comments
    
    * Update plugins/modules/sonic_ptp_default_ds.py
    
    Correct grammar for this description.
    
    * Update plugins/modules/sonic_ptp_default_ds.py
    
    Correct grammar for this description.
    
    ---------
    
    Co-authored-by: Kerry Meyer <kerry.meyer@dell.com>

--- plugins/module_utils/network/sonic/facts/facts.py
+++ plugins/module_utils/network/sonic/facts/facts.py
@@ -65,6 +65,7 @@ from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.s
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.copp.copp import CoppFacts
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.route_maps.route_maps import Route_mapsFacts
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.lldp_interfaces.lldp_interfaces import Lldp_interfacesFacts
+from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.ptp_default_ds.ptp_default_ds import Ptp_default_dsFacts
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.stp.stp import StpFacts
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.sflow.sflow import SflowFacts
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.facts.fips.fips import FipsFacts
@@ -117,6 +118,7 @@ FACT_RESOURCE_SUBSETS = dict(
     users=UsersFacts,
     system=SystemFacts,
     port_breakout=Port_breakoutFacts,
+    ptp_default_ds=Ptp_default_dsFacts,
     aaa=AaaFacts,
     ldap=LdapFacts,
     tacacs_server=Tacacs_serverFacts,
