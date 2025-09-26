320761a9225e02c7ebc56c35c339023669e8576e
stalabi1 <54641848+stalabi1@users.noreply.github.com>
2025-09-26 16:32:18 -0700

    Add FBS groups resource module (#480)
    * Add FBS groups resource module
    
    * Add overridden UT test case
    
    * Update sonic_fbs_groups.py
    
    * Add hyphens to descriptions
    
    * Fix ansible-lint errors
    
    * Update overridden and deleted states
    
    * Update preparation.yaml
    
    * Fix ansible-lint errors and update copyright
    
    * Update false bool handling
    
    * Update sonic_fbs_groups.py
    
    * Update fbs groups module
    
    * Correct typo
    
    * Update descriptions
    
    * Fix typo
    
    * Add new line as per Austin's request
    
    * Add missing conditonal
    
    * Update code comments
    
    * Update check mode
    
    * Clean up code
    
    * Fix sanity error
    
    * Add symlink
    
    * Make methods static
    
    * Rework
    
    * Address Arun's review comments

--- plugins/module_utils/network/sonic/argspec/facts/facts.py
+++ plugins/module_utils/network/sonic/argspec/facts/facts.py
@@ -92,6 +92,7 @@ class FactsArgs(object):  # pylint: disable=R0903
         'ssh',
         'lst',
         'fbs_classifiers',
+        'fbs_groups',
         'fbs_policies',
         'ars',
         'mirroring',
