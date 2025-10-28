1beeef8454b0849a65ce30e4a67c31ec475c63d5
stalabi1 <54641848+stalabi1@users.noreply.github.com>
2025-10-28 15:16:42 -0700

    Update sonic.py

--- plugins/modules/sonic.py
+++ plugins/modules/sonic.py
@@ -38,8 +38,7 @@ def main():
                            supports_check_mode=True)
     # The action plugin will handle the execution.
     # Execution of this module is not expected when plugins are operating correctly.
-    module.exit_json(changed=False, msg="Unexpected invocation of the sonic.py module. Instead
-                                of this module, the referring plugin should have been executed.")
+    module.exit_json(changed=False, msg="Unexpected invocation of the sonic.py module. Instead of this module, the referring plugin should have been executed.")
 
 
 if __name__ == '__main__':
