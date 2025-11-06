4c4907cb6713023cd61da05bed4ed1da324b7a1a
Kerry Meyer <kerry.meyer@dell.com>
2025-11-06 17:24:17 -0500

    Implement changes for compatibility with ansible-core 2.19

--- tests/unit/modules/utils.py
+++ tests/unit/modules/utils.py
@@ -9,16 +9,38 @@ from ansible.module_utils._text import to_bytes
 from ansible_collections.dellemc.enterprise_sonic.tests.unit.compat import unittest
 from ansible_collections.dellemc.enterprise_sonic.tests.unit.compat.mock import patch
 
+cur_context = None
 
 def set_module_args(args):
+
+    global cur_context
+
     if '_ansible_remote_tmp' not in args:
         args['_ansible_remote_tmp'] = '/tmp'
     if '_ansible_keep_remote_files' not in args:
         args['_ansible_keep_remote_files'] = False
 
-    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
-    basic._ANSIBLE_ARGS = to_bytes(args)
+    # Quiesce and clear any previously existing context for
+    # patch_module_args invocation.
+    if cur_context is not None:
+        try:
+            cur_context.__exit__(None, None, None)
+        except Exception:
+            pass
+        cur_context = None
 
+    # Use patch_module_args if it's available (used in
+    # ansible-core 2.19+)
+    try:
+        from ansible.module_utils.testing import patch_module_args
+
+        cur_context = patch_module_args(args)
+        cur_context.__enter__()
+    except ImportError:
+        # Fall back to "manual" setting of module args for older
+        # Ansible versions.
+        serialized_args = json.dumps({"ANSIBLE_MODULE_ARGS": args})
+        basic._ANSIBLE_ARGS = to_bytes(serialized_args)
 
 class AnsibleExitJson(Exception):
     pass
