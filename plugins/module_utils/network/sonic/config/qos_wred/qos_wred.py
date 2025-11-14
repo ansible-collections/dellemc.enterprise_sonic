083ec34fad2ad6cadd8dfe6aff15249fba46d2e7
Shade Talabi <shade_talabi@dell.com>
2025-11-14 15:53:29 -0800

    Fix check mode and refactor code for QoS WRED module

--- plugins/module_utils/network/sonic/config/qos_wred/qos_wred.py
+++ plugins/module_utils/network/sonic/config/qos_wred/qos_wred.py
@@ -27,7 +27,7 @@ from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.s
     update_states
 )
 from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.sonic.utils.formatted_diff_utils import (
-    __DELETE_CONFIG_IF_NO_SUBCONFIG,
+    __DELETE_OP_DEFAULT,
     get_new_config,
     get_formatted_config_diff
 )
@@ -38,15 +38,30 @@ from ansible_collections.dellemc.enterprise_sonic.plugins.module_utils.network.s
 )
 
 
+delete_all = False
 QOS_WRED_PATH = '/data/openconfig-qos:qos/wred-profiles/wred-profile'
 PATCH = 'patch'
 DELETE = 'delete'
 TEST_KEYS = [
     {'config': {'name': ''}}
 ]
-TEST_KEYS_formatted_diff = [
-    {'config': {'name': '', '__delete_op': __DELETE_CONFIG_IF_NO_SUBCONFIG}}
+
+
+def __derive_delete_op(key_set, command, exist_conf):
+    """Returns new QoS WRED configuration for delete operation"""
+    if delete_all:
+        return True, {}
+
+    return __DELETE_OP_DEFAULT(key_set, command, exist_conf)
+
+
+TEST_KEYS_generate_config = [
+    {'config': {'name': '', '__delete_op': __derive_delete_op}}
 ]
+enum_dict = {
+    'all': 'ECN_ALL',
+    'none': 'ECN_NONE'
+}
 
 
 class Qos_wred(ConfigBase):
@@ -69,8 +84,8 @@ class Qos_wred(ConfigBase):
     def get_qos_wred_facts(self):
         """ Get the 'facts' (the current configuration)
 
-        :rtype: A dictionary
-        :returns: The current configuration as a dictionary
+        :rtype: A list
+        :returns: The current configuration as a list
         """
         facts, _warnings = Facts(self._module).get_facts(self.gather_subset, self.gather_network_resources)
         qos_wred_facts = facts['ansible_network_resources'].get('qos_wred')
@@ -85,138 +100,73 @@ class Qos_wred(ConfigBase):
         :returns: The result from module execution
         """
         result = {'changed': False}
-        warnings = []
         commands = []
 
         existing_qos_wred_facts = self.get_qos_wred_facts()
         commands, requests = self.set_config(existing_qos_wred_facts)
-        if commands and len(requests) > 0:
+        if commands and requests:
             if not self._module.check_mode:
                 try:
                     edit_config(self._module, to_request(self._module, requests))
                 except ConnectionError as exc:
                     self._module.fail_json(msg=str(exc), code=exc.code)
             result['changed'] = True
+
         result['commands'] = commands
-
-        changed_qos_wred_facts = self.get_qos_wred_facts()
-
         result['before'] = existing_qos_wred_facts
-        if result['changed']:
-            result['after'] = changed_qos_wred_facts
-
-        new_config = changed_qos_wred_facts
         old_config = existing_qos_wred_facts
+
         if self._module.check_mode:
-            result.pop('after', None)
-            new_config = get_new_config(commands, existing_qos_wred_facts,
-                                        TEST_KEYS_formatted_diff)
+            new_config = get_new_config(commands, existing_qos_wred_facts, TEST_KEYS_generate_config)
+            self.sort_lists_in_config(new_config)
             result['after(generated)'] = new_config
+        else:
+            new_config = self.get_qos_wred_facts()
+            if result['changed']:
+                result['after'] = new_config
         if self._module._diff:
             self.sort_lists_in_config(new_config)
             self.sort_lists_in_config(old_config)
-            result['diff'] = get_formatted_config_diff(old_config,
-                                                       new_config,
-                                                       self._module._verbosity)
+            result['diff'] = get_formatted_config_diff(old_config, new_config, self._module._verbosity)
 
-        result['warnings'] = warnings
         return result
 
     def set_config(self, existing_qos_wred_facts):
         """ Collect the configuration from the args passed to the module,
-            collect the current configuration (as a dict from facts)
+            collect the current configuration (as a list from facts)
 
         :rtype: A list
         :returns: the commands necessary to migrate the current configuration
                   to the desired configuration
         """
         want = remove_empties_from_list(self._module.params['config'])
-        have = remove_empties_from_list(existing_qos_wred_facts)
+        have = existing_qos_wred_facts
+        self.sort_lists_in_config(want)
+        self.sort_lists_in_config(have)
         resp = self.set_state(want, have)
         return to_list(resp)
 
     def set_state(self, want, have):
         """ Select the appropriate function based on the state provided
 
-        :param want: the desired configuration as a dictionary
-        :param have: the current configuration as a dictionary
+        :param want: the desired configuration as a list
+        :param have: the current configuration as a list
         :rtype: A list
         :returns: the commands necessary to migrate the current configuration
                   to the desired configuration
         """
-        commands = []
-        requests = []
+        commands, requests = [], []
         state = self._module.params['state']
         diff = get_diff(want, have, TEST_KEYS)
 
-        if state == 'overridden':
-            commands, requests = self._state_overridden(want, have)
-        elif state == 'deleted':
-            commands, requests = self._state_deleted(want, have)
-        elif state == 'merged':
+        if state == 'merged':
             commands, requests = self._state_merged(diff)
         elif state == 'replaced':
             commands, requests = self._state_replaced(want, have, diff)
-        return commands, requests
-
-    def _state_replaced(self, want, have, diff):
-        """ The command generator when state is replaced
-
-        :rtype: A list
-        :returns: the commands necessary to migrate the current configuration
-                  to the desired configuration
-        """
-        commands = []
-        mod_commands = []
-        requests = []
-        replaced_config = self.get_replaced_config(want, have)
-
-        if replaced_config:
-            self.sort_lists_in_config(replaced_config)
-            self.sort_lists_in_config(have)
-            is_delete_all = replaced_config == have
-            del_requests = self.get_delete_qos_wred_requests(replaced_config, have, is_delete_all)
-            requests.extend(del_requests)
-            commands.extend(update_states(replaced_config, 'deleted'))
-            mod_commands = want
-        else:
-            mod_commands = diff
-
-        if mod_commands:
-            mod_request = self.get_modify_qos_wred_request(mod_commands)
-
-            if mod_request:
-                requests.append(mod_request)
-                commands.extend(update_states(mod_commands, 'replaced'))
-        return commands, requests
-
-    def _state_overridden(self, want, have):
-        """ The command generator when state is overridden
-
-        :rtype: A list
-        :returns: the commands necessary to migrate the current configuration
-                  to the desired configuration
-        """
-        commands = []
-        requests = []
-        self.sort_lists_in_config(want)
-        self.sort_lists_in_config(have)
-
-        if have and have != want:
-            is_delete_all = True
-            del_requests = self.get_delete_qos_wred_requests(have, None, is_delete_all)
-            requests.extend(del_requests)
-            commands.extend(update_states(have, 'deleted'))
-            have = []
-
-        if not have and want:
-            mod_commands = want
-            mod_request = self.get_modify_qos_wred_request(mod_commands)
-
-            if mod_request:
-                requests.append(mod_request)
-                commands.extend(update_states(mod_commands, 'overridden'))
-
+        elif state == 'overridden':
+            commands, requests = self._state_overridden(want, have, diff)
+        elif state == 'deleted':
+            commands, requests = self._state_deleted(want, have, diff)
         return commands, requests
 
     def _state_merged(self, diff):
@@ -229,39 +179,97 @@ class Qos_wred(ConfigBase):
         commands = diff
         requests = self.get_modify_qos_wred_request(commands)
 
-        if commands and len(requests) > 0:
+        if commands and requests:
             commands = update_states(commands, 'merged')
         else:
             commands = []
 
         return commands, requests
 
-    def _state_deleted(self, want, have):
+    def _state_replaced(self, want, have, diff):
+        """ The command generator when state is replaced
+
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        commands, mod_commands = [], []
+        replaced_config, requests = self.get_replaced_config(want, have)
+
+        if replaced_config:
+            commands.extend(update_states(replaced_config, 'deleted'))
+            mod_commands = want
+        else:
+            mod_commands = diff
+
+        if mod_commands:
+            mod_request = self.get_modify_qos_wred_request(mod_commands)
+
+            if mod_request:
+                requests.append(mod_request)
+                commands.extend(update_states(mod_commands, 'replaced'))
+
+        return commands, requests
+
+    def _state_overridden(self, want, have, diff):
+        """ The command generator when state is overridden
+
+        :rtype: A list
+        :returns: the commands necessary to migrate the current configuration
+                  to the desired configuration
+        """
+        global delete_all
+        delete_all = False
+        commands, requests = [], []
+        mod_commands, mod_request = None, None
+        del_commands = get_diff(have, want, TEST_KEYS)
+
+        if del_commands:
+            delete_all = True
+            del_requests = self.get_delete_qos_wred_requests(del_commands, delete_all)
+            requests.extend(del_requests)
+            commands.extend(update_states(have, 'deleted'))
+            mod_commands = want
+            mod_request = self.get_modify_qos_wred_request(mod_commands)
+        elif diff:
+            mod_commands = diff
+            mod_request = self.get_modify_qos_wred_request(mod_commands)
+
+        if mod_request:
+            requests.append(mod_request)
+            commands.extend(update_states(mod_commands, 'overridden'))
+
+        return commands, requests
+
+    def _state_deleted(self, want, have, diff):
         """ The command generator when state is deleted
 
         :rtype: A list
         :returns: the commands necessary to remove the current configuration
                   of the provided objects
         """
-        is_delete_all = False
+        global delete_all
+        delete_all = False
+        commands, requests = [], []
 
         if not want:
             commands = deepcopy(have)
-            is_delete_all = True
+            delete_all = True
         else:
-            commands = deepcopy(want)
+            commands = get_diff(want, diff, TEST_KEYS)
 
-        requests = self.get_delete_qos_wred_requests(commands, have, is_delete_all)
-
-        if commands and len(requests) > 0:
-            commands = update_states(commands, 'deleted')
+        if commands:
+            requests = self.get_delete_qos_wred_requests(commands, delete_all)
+            if requests:
+                commands = update_states(commands, 'deleted')
         else:
             commands = []
+
         return commands, requests
 
     def get_modify_qos_wred_request(self, commands):
+        """ Returns a patch request to modify the QoS WRED configuration"""
         request = None
-        lookup_dict = {'all': 'ECN_ALL', 'none': 'ECN_NONE'}
 
         if commands:
             wred_list = []
@@ -270,6 +278,7 @@ class Qos_wred(ConfigBase):
                 name = wred.get('name')
                 ecn = wred.get('ecn')
                 colors = ['green', 'red', 'yellow']
+
                 for color in colors:
                     color_dict = wred.get(color)
                     if color_dict:
@@ -277,6 +286,7 @@ class Qos_wred(ConfigBase):
                         min_threshold = color_dict.get('min_threshold')
                         max_threshold = color_dict.get('max_threshold')
                         drop_probability = color_dict.get('drop_probability')
+
                         if enable is not None:
                             config_dict[f'wred-{color}-enable'] = enable
                         if min_threshold:
@@ -286,7 +296,7 @@ class Qos_wred(ConfigBase):
                         if drop_probability is not None:
                             config_dict[f'{color}-drop-probability'] = str(drop_probability)
                 if ecn:
-                    config_dict['ecn'] = lookup_dict[ecn]
+                    config_dict['ecn'] = enum_dict[ecn]
                 if name:
                     config_dict['name'] = name
                     wred_list.append({'name': name, 'config': config_dict})
@@ -296,78 +306,79 @@ class Qos_wred(ConfigBase):
 
         return request
 
-    def get_delete_qos_wred_requests(self, commands, have, is_delete_all):
+    def get_delete_qos_wred_requests(self, commands, is_delete_all):
+        """Returns a list of delete requests to delete the specified QoS WRED configuration"""
         requests = []
 
         if not commands:
             return requests
 
         if is_delete_all:
-            requests.append({'path': QOS_WRED_PATH, 'method': DELETE})
+            requests.append(self.get_delete_qos_wred_request())
             return requests
 
-        config_list = []
-        colors = ['green', 'red', 'yellow']
-        attributes = ['enable', 'min_threshold', 'max_threshold', 'drop_probability']
+        colors = ('green', 'red', 'yellow')
+        attributes = ('enable', 'min_threshold', 'max_threshold', 'drop_probability')
+
         for wred in commands:
             name = wred.get('name')
+            if len(wred) == 1:
+                requests.append(self.get_delete_qos_wred_request(name))
+                continue
+
             ecn = wred.get('ecn')
-            for cfg_wred in have:
-                cfg_name = cfg_wred.get('name')
-                cfg_ecn = cfg_wred.get('ecn')
-                if name == cfg_name:
-                    wred_dict = {}
-                    if ecn and ecn == cfg_ecn:
-                        requests.append(self.get_delete_wred_cfg_attr(name, 'ecn'))
-                        wred_dict.update({'name': name, 'ecn': ecn})
-                    for color in colors:
-                        color_dict = wred.get(color)
-                        cfg_color_dict = cfg_wred.get(color)
-                        color_attr_dict = {}
-                        if color_dict and cfg_color_dict:
-                            for attr in attributes:
-                                val = color_dict.get(attr)
-                                cfg_val = cfg_color_dict.get(attr)
-                                if val is not None and val == cfg_val:
-                                    if attr == 'enable':
-                                        requests.append(self.get_delete_wred_cfg_attr(name, f'wred-{color}-{attr}'))
-                                    else:
-                                        requests.append(self.get_delete_wred_cfg_attr(name, f'{color}-{attr.replace("_", "-")}'))
-                                color_attr_dict[attr] = val
-                        if color_attr_dict:
-                            wred_dict[color] = color_attr_dict
+            if ecn:
+                requests.append(self.get_delete_qos_wred_request(name, 'ecn'))
+            for color in colors:
+                color_dict = wred.get(color)
+                if color_dict:
+                    for attr in attributes:
+                        val = color_dict.get(attr)
+                        if val is not None:
+                            oc_attr = None
+                            if attr == 'enable':
+                                oc_attr = f'wred-{color}-{attr}'
+                            else:
+                                oc_attr = f'{color}-{attr.replace("_", "-")}'
+                            requests.append(self.get_delete_qos_wred_request(name, oc_attr))
 
-                    if not ecn and not any(wred.get(color) for color in colors):
-                        url = '%s=%s' % (QOS_WRED_PATH, name)
-                        requests.append({'path': url, 'method': DELETE})
-                        wred_dict['name'] = name
-
-                    if wred_dict:
-                        config_list.append(wred_dict)
-                    break
-
-        commands = config_list
         return requests
 
-    def sort_lists_in_config(self, config):
+    @staticmethod
+    def sort_lists_in_config(config):
+        """Sorts the lists in QoS WRED configuration"""
         if config:
             config.sort(key=lambda x: x['name'])
 
-    def get_delete_wred_cfg_attr(self, name, attr):
-        url = '%s=%s/config/%s' % (QOS_WRED_PATH, name, attr)
+    @staticmethod
+    def get_delete_qos_wred_request(name=None, attr=None):
+        """Returns a delete request to delete the specified QoS WRED configuration"""
+        url = QOS_WRED_PATH
+        if name:
+            url += f'={name}'
+        if attr:
+            url += f'/config/{attr}'
         request = {'path': url, 'method': DELETE}
 
         return request
 
     def get_replaced_config(self, want, have):
-        config_list = []
+        """Returns the replaced QoS WRED configuration and the corresponding delete requests"""
+        config_list, requests = [], []
 
+        if not want or not have:
+            return config_list, requests
+
+        cfg_wred_dict = {wred.get('name'): wred for wred in have}
         for wred in want:
             name = wred.get('name')
-            for cfg_wred in have:
-                cfg_name = cfg_wred.get('name')
-                if name == cfg_name:
-                    if wred != cfg_wred:
-                        config_list.append({'name': cfg_name})
+            cfg_wred = cfg_wred_dict.get(name)
 
-        return config_list
+            if not cfg_wred:
+                continue
+
+            if wred != cfg_wred:
+                requests.append(self.get_delete_qos_wred_request(name))
+                config_list.append(cfg_wred)
+
+        return config_list, requests
